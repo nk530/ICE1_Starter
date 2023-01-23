a92a9dc36cd82fde5ee29948eac605728bbc1eec
nk530 <nevishku@sheridancollege.ca>
2023-01-23 14:00:10 -0500

    ICE1 Version 1

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -16,6 +16,7 @@ public class CardTrick {
 
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
+            System.out.println("Hello");
             //card.setValue(insert call to random number generator here)
             // 
             //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
